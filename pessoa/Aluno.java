65aee8b6f10dbfb6c47f7d4028c4b58fcb90f25f
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-18 02:33:18 -0300

    Add Aluno class and TesteDePessoa updates

--- pessoa/Aluno.java
+++ pessoa/Aluno.java
@@ -1,5 +1,18 @@
 package pessoa;
 
-public class Aluno {
-  
+public class Aluno extends Pessoa {
+  private String matricula;
+
+  public String getMatricula() {
+    return matricula;
+  }
+
+  public void setMatricula(String matricula) {
+    this.matricula = matricula;
+  }
+
+  public Aluno(String nome, int idade, String cpf, String matricula) {
+    super(nome, idade, cpf);
+    this.matricula = matricula;
+  }
 }
--- pessoa/TesteDePessoa.java
+++ pessoa/TesteDePessoa.java
@@ -2,8 +2,13 @@ package pessoa;
 
 public class TesteDePessoa {
   public static void main(String[] args) {
-    Pessoa pessoa = new Pessoa("João", 20, "123.456.789-00");
+    Professor professor = new Professor("João", 30, "123.456.789-00", 1000.0);
+    Aluno aluno = new Aluno("Maria", 20, "987.654.321-00", "20190001");
 
-    pessoa.imprimirDados();
+    professor.imprimirDados();
+    System.out.println("Salário: " + professor.getSalario());
+
+    aluno.imprimirDados();
+    System.out.println("Matrícula: " + aluno.getMatricula());
   }
 }
