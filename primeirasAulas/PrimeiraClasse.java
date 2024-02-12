115cff1bf181419913a54d7f0bc858baffe559ad
Tamicktom <henrique.angelo@enterscience.com.br>
2024-02-12 01:01:11 -0300

    asdasd

--- primeirasAulas/PrimeiraClasse.java
+++ primeirasAulas/PrimeiraClasse.java
@@ -4,11 +4,16 @@ package primeirasAulas;
  * PrimeiraClasse = nome da classe
  * public = modificador de acesso, tipo de acesso
  * class = palavra reservada para definir uma classe
+ * 
+ * @deprecated
  */
 public class PrimeiraClasse {
   // todo código Java deve estar dentro de uma classe
+  int idade = 10; // atributo
+  String nome = "João"; // atributo
+
   public static void main(String[] args) {
 
-    
+    System.out.println("Olá, mundo!");
   }
 }
